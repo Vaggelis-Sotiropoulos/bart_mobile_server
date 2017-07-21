b7911476394c9adf5352957741b4521ab34459ef
Vaggelis Sotiropoulos <vsotiropoulo@gmail.com>
2017-07-21 01:37:12 -0700

    Moves scheduler.js to appropriate location

--- server/workers/scheduler.js
+++ server/workers/scheduler.js
@@ -1,5 +1,5 @@
 var schedule = require('node-schedule');
-const db = require('knex')(require('../knexfile.js')); //change knexfile location accordingly
+const db = require('knex')(require('../knexfile.js')); 
 const config = require('config');
 const env = require('dotenv').config();
 
